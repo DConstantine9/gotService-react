98efcf58f29a4ee6617b899adc3aa63dbc5fbbac
const <33183008+DConstantine9@users.noreply.github.com>
2022-05-31 13:25:37 +0300

    BooksItem

---
+++ src/components/pages/characterPage.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react'
+import GotService from '../../services/gotService'
+import ErrorMessage from '../error'
+import ItemList from '../itemList'
+import ItemDetails, {Field} from '../itemDetails'
+import RowBlock from "../rowBlock"
+
+export default class CharacterPage extends Component {
+  state = {
+    selectedChar: 130,
+    error: false
+  }
+
+  gotService = new GotService()
+
+  componentDidCatch() {
+    this.setState({
+      error: true
+    })
+  }
+
+  onItemSelected = (id) => {
+    this.setState({
+      selectedChar: id 
+    })
+  }
+
+  render() {
+    
+    if (this.state.error) {
+      return <ErrorMessage />
+    }
+
+    const itemList = (
+      <ItemList
+        getData={this.gotService.getAllCharacters}
+        onItemSelected={this.onItemSelected} 
+        renderItem={item => `${item.name} (${item.gender})`}
+      />
+    )
+
+    const personDetails = (
+      <ItemDetails charId={this.state.selectedChar}>
+        <Field field="gender" label="gender" />
+        <Field field="born" label="born" />
+        <Field field="died" label="died" />
+        <Field field="culture" label="culture" />
+      </ItemDetails>
+    )
+
+    return (
+      <RowBlock left={itemList} right={personDetails} /> 
+    )
+  }
+}
---
+++ src/components/spinner/spinner.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./spinner.css"
+
+export default function spinner() {
+  return (
+    <div className="loadingio-spinner-ripple-ud74lv0dhqj">
+      <div className="ldio-g5vwsw7r9jo">
+        <div></div><div></div>
+      </div>
+    </div>
+  )
+}
+
+
+
